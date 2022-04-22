00479a417e4100243aebec539ae73228e47e1b32
pablogqipv <pablo.garcia@iusspavia.it>
2022-04-22 15:10:47 +0200

    changed stuffs

--- Plot3D.py
+++ Plot3D.py
@@ -9,7 +9,7 @@ fig = plt.figure()
 ax = fig.gca(projection='3d')
  
 # 1D arrays for x and y coordinates
-n_points = 200
+n_points = 100
 x_min,x_max = -5,5
 y_min,y_max = -5,5
 X = np.linspace(x_min,x_max, n_points)
@@ -19,11 +19,11 @@ Y = np.linspace(y_min,y_max, n_points)
 X, Y = np.meshgrid(X, Y)
  
 # Z 2D grid for the plot
-R = np.sqrt(X**2 + Y**2)
+R = np.sqrt(X**3 + Y**2)
 Z = np.sin(R)
  
 # plot the surface.
-surf = ax.plot_surface(X, Y, Z, cmap='magma',linewidth=0, antialiased=True)
+surf = ax.plot_surface(X, Y, Z, cmap='viridis',linewidth=0, antialiased=True)
  
 # Customize the z axis.
 ax.set_zlim(-1.01, 1.01)
